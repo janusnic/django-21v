01ab7e74425d75d945e18d24828dd48e69d2a268
Janus Nicon <janusnic@gmail.com>
2015-07-12 22:23:12 +0300

    Basic view now returns minimal HTML

--- unit_12/mysite/blog/views.py
+++ unit_12/mysite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+#  home_page = None
+def home_page(request):
+    return HttpResponse('<html><title>Blog lists</title></html>')
+
+# "You must be joking!" I can hear you say.
